df0e78fe8abc4bf9e92815fac766b65e8e2a8d96
v-chuqin <qchys@hotmail.com>
2016-10-11 18:20:07 +0900

    update

--- DataPreprocess/mdata.py
+++ DataPreprocess/mdata.py
@@ -43,9 +43,9 @@ def handleTweet(type,threshold):
 		file = open(os.getcwd()+'/../Data/dataset-lmarujo-ACL2015/'+type+'/tweet.'+type+'.tok.en-'+str(fileIndex)+'.txt')
 		line = file.readline()
 		cleanline = cleanEnglishString(line,Stopwords)
-		print str(fileIndex) +' : ' + cleanline
+		print str(fileIndex) +' : ' + cleanline.lower()
 		# set_trace()
-		tmp_list = cleanline.split(' ')
+		tmp_list = cleanline.lower().split(' ')
 		if len(tmp_list) == 1:
 			continue
 		Tweets.append(tmp_list)
@@ -61,25 +61,58 @@ def handleTweet(type,threshold):
 			value = int(_tmp_list[1])
 			if value >= threshold and word in tmp_list:
 				words_tmp.append(word)
-		Keywords.append(words_tmp)
 		print words_tmp
+		# if len(words_tmp) >= threshold:
+		Keywords.append(words_tmp)
+		# Tweets.append(tmp_list)
 	return Tweets,Keywords
 
 def getTweetfile(threshold):
+	# file = open(os.getcwd()+'/../Data/tweets.txt','w')
 	file = open(os.getcwd()+'/../Data/tweets.threshold_'+str(threshold)+'.txt','w')
+	filekey = open(os.getcwd()+'/../Data/tweetskey.threshold_'+str(threshold)+'.txt','w')
 	Tweets,Keywords = handleTweet('train',threshold)
 	for i in range(0,len(Tweets)):
 		line = ''
 		for j in range(0,len(Tweets[i])):
 			line = line + ' ' + Tweets[i][j]
 		file.write(line[1:]+'\n')
+	for i in range(0,len(Keywords)):
+		line = ''
+		for j in range(0,len(Keywords[i])):
+			line = line + ' ' + Keywords[i][j]
+		filekey.write(line[1:]+'\n')
+
 	Tweets,Keywords = handleTweet('test',threshold)
 	for i in range(0,len(Tweets)):
 		line = ''
 		for j in range(0,len(Tweets[i])):
 			line = line + ' ' + Tweets[i][j]
 		file.write(line[1:]+'\n')
+	for i in range(0,len(Keywords)):
+		line = ''
+		for j in range(0,len(Keywords[i])):
+			line = line + ' ' + Keywords[i][j]
+		filekey.write(line[1:]+'\n')
+
 	Tweets,Keywords = handleTweet('dev',threshold)
+	for i in range(0,len(Tweets)):
+		line = ''
+		for j in range(0,len(Tweets[i])):
+			line = line + ' ' + Tweets[i][j]
+		file.write(line[1:]+'\n')
+	for i in range(0,len(Keywords)):
+		line = ''
+		for j in range(0,len(Keywords[i])):
+			line = line + ' ' + Keywords[i][j]
+		filekey.write(line[1:]+'\n')
+
+	file.close()
+	filekey.close()
+
+def generateTweetfile(threshold,type):
+	file = open(os.getcwd()+'/../Data/tweets.'+type+'.threshold_'+str(threshold)+'.txt','w')
+	Tweets,Keywords = handleTweet(type,threshold)
 	for i in range(0,len(Tweets)):
 		line = ''
 		for j in range(0,len(Tweets[i])):
@@ -87,6 +120,8 @@ def getTweetfile(threshold):
 		file.write(line[1:]+'\n')
 	file.close()
 
+# getTweetfile(7)
 getTweetfile(6)
-
+# generateTweetfile(6,'train')
+# generateTweetfile(6,'test')
 
--- embedding/retrainWord2vec.py
+++ embedding/retrainWord2vec.py
@@ -2,6 +2,9 @@ from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 from ipdb import set_trace
 from multiprocessing import cpu_count
+from copy import deepcopy
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 # waiting for the gensim udpate, support the new vocabulary to the pretrain model
@@ -11,27 +14,35 @@ def retrainWord2vec(pretrainModel,trainfile):
 	set_trace()
 	# model.update_vocab(new_sentences)
 	premodel.build_vocab(sentences, update=True)
-	premodel.train(sentences,min_count=0,workers=cpu_count())
+	premodel.train(sentences)
 	# model = premodel.train(sentences,size=50,window=5,min_count=1,workers=cpu_count())
 	premodel.save_word2vec_format('../Data/'+trainfile+ '.retrian.model.bin', binary=True)
 	return premodel
 
 def trainWord2vec(trainfile):
 	sentences = LineSentence('../Data/'+trainfile)
-	model = Word2Vec(sentences,size=50,window=5,min_count=1,workers=cpu_count())
+	model = Word2Vec(sentences,size=50,window=3,min_count=0,workers=cpu_count())
+	model.save('../Data/'+trainfile+ '.model')
 	model.save_word2vec_format('../Data/'+trainfile+ '.model.bin', binary=True)
 	return model
 
-def test(trainfile):
-	sentences = LineSentence('../Data/'+trainfile)
-	model = Word2Vec(sentences,size=50,window=5,min_count=0,workers=cpu_count())
-	model.build_vocab(sentences, update=True)
-	model.train(sentences,min_count=0,workers=cpu_count())
+def test(trainfile,testfile):
+	trainsentences = LineSentence('../Data/'+trainfile)
+	testsentences = LineSentence('../Data/'+testfile)
+	# model = Word2Vec(trainsentences,size=50,window=5,min_count=0,workers=cpu_count())
+	model = Word2Vec.load('oldmodel')
+	oldmodel = deepcopy(model)
+	# oldmodel.save('oldmodel')
+	model.build_vocab(testsentences, update=True)
+	model.train(testsentences)
+	model.save('newmodel')
+	for m in ['oldmodel', 'model']:
+		print 'The vocabulary size of the' +m+ 'is ' + str(len(eval(m).vocab)) 
 	return model
 
 
 model = trainWord2vec('tweets.threshold.txt')
 # model = retrainWord2vec('GoogleNews-vectors-negative300.bin','tweets.threshold.txt')
-# model = test('tweets.threshold.txt')
+# model = test('tweets.train.threshold_6.txt','tweets.test.threshold_6.txt')
 set_trace()
 
